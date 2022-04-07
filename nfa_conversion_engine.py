c32858559826ff420037202f3833a465f4581d11
Radu Vrinceanu <radu.vrinceanu@ivfuture.uk>
2022-04-07 17:46:34 +0300

    added print to config file

--- nfa_conversion_engine.py
+++ nfa_conversion_engine.py
@@ -69,6 +69,12 @@ def dfa_conversion(sigma, delta, states):
     return {"states": new_states, "delta": new_delta}
 
 
+def showCompositeState(state):
+    if len(state) > 1:
+        string = "(" + ", ".join([i for i in state]) + ")"
+    else:
+        string = state[0]
+    return string
 
 
 if __name__ == "__main__":
@@ -79,10 +85,32 @@ if __name__ == "__main__":
 
     try:
         reading(sigma, delta, states, args.file)
-        print(f"SIGMA: {sigma}")
         dfa_data = dfa_conversion(sigma, delta, states)
-        print(f"STATES: {dfa_data['states']}")
-        print(f"DELTA: {dfa_data['delta']}")
-        print("State which is noted with S or S/F means that is the start state, states noted with I are intermediate and states noted with F or S/F are final.")
+
+        f = open("nfa_converted_config", "w")
+        f.write("Sigma:\n")
+        for i in sigma:
+            f.write(f"{i}\n")
+        f.write("End\n\n")
+        f.write("States:\n")
+        for states in dfa_data['states']:
+            shown = showCompositeState(states)
+            if dfa_data['states'][states] == "I":
+                f.write(f"{shown}\n")
+            elif dfa_data['states'][states] == "S/F":
+                f.write(f"{shown}, S, F\n")
+            elif dfa_data['states'][states] == "F":
+                f.write(f"{shown}, F\n")
+            elif dfa_data['states'][states] == "S":
+                f.write(f"{shown}, S\n")
+        f.write("End\n\n")
+        f.write("Transitions:\n")
+        for which_state in dfa_data['delta']:
+            which_shown = showCompositeState(which_state)
+            for to_go_state in dfa_data['delta'][which_state]:
+                to_go_shown = showCompositeState(to_go_state)
+                for k in dfa_data['delta'][which_state][to_go_state]:
+                    f.write(f"{which_shown}, {k}, {to_go_shown}\n")
+        f.write("End\n\n")
     except Exception as exception:
         print(exception.args)
